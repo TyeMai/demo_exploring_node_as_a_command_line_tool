2d78b17624c1199cc97ca1e9f41ae8a29d7e32a4
Bideo Wego <bideowego@gmail.com>
2017-03-27 17:04:10 -0400

    Add error listener to child_process.spawn example

--- child_processes/spawn.js
+++ child_processes/spawn.js
@@ -3,13 +3,16 @@ var cp = require('child_process');
 
 var cmd = cp.spawn('echo', ['Hello world!']);
 
+cmd.on('error', (err) => {
+  console.error(`${ err.stack }`);
+});
 
 cmd.stdout.on('data', (data) => {
   console.log(`Data: ${ data }`);
 });
 
 cmd.stderr.on('data', (data) => {
-  console.error(`Error: ${ data }`);
+  console.error(`STDERR: ${ data }`);
 });
 
 cmd.on('close', (code) => {
